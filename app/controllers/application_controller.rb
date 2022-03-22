53da3653f7a3aae0a312a9ec91aead8173f004fa
remi-1201 <remi.nkjm@gmail.com>
2022-03-22 20:06:09 +0900

    Devideユザー登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  
+  # deviseのストロングパラメータ設定
+  protected
+  def configure_permitted_parameters
+    # sign_up時に[:name]の値を許す設定
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
